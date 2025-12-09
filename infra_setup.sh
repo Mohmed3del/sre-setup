d8981d386d588e4043c0b950bf483b340e2182ab
Mohmed Adel <mohmed3del1@gmail.com>
2025-12-09 03:53:06 +0000

    add script to build infra

---
+++ infra_setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+PS3="please enter your choice number :" 
+select var in plan apply configure destroy
+do
+case   $REPLY in 
+plan|1)
+	terraform -chdir=./Terraform init \
+    && terraform -chdir=./Terraform plan ;break;;
+    
+apply|2)
+    terraform -chdir=./Terraform init \
+    && terraform -chdir=./Terraform apply --auto-approve  ;break   ;;
+    
+
+destroy|3)
+	terraform -chdir=./Terraform destroy --auto-approve ;break ;;
+    
+*)
+echo "$REPLY is not valid choice"	
+esac	
+done
